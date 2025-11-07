f81c642a60c8095cf06d3a6297a1f8474bddfd01
guptaanushtha94-1 <guptaanushtha94@gmail.com>
2025-11-07 17:12:11 +0530

    Update test_lab01.py

--- test_lab01.py
+++ test_lab01.py
@@ -3,7 +3,7 @@ def test_a1():
     assert 1+1==2
 def test_a2():
     print("this is first")
-    assert 1/2==1,"failed case"
+    assert 1/1==1,"failed case"
 @pytest.mark.skip(reason="skipping intentionally")
 def test_a3():
     assert 9//5==1
